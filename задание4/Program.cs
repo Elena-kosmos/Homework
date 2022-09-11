7b62f22c9ac8c383957e689b387a0928b047149d
Elena <eutereshkova@mail.ru>
2022-09-11 20:27:47 +0300

    добавили задачу 3

---
+++ задание4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите  число: ");
+int number1 = int.Parse(Console.ReadLine());
+if(number1 %2 == 0)
+{
+    Console.Write("четное число");
+}
+else
+{
+        Console.Write("нечетное число");
+}
